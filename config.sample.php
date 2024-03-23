8b68a8aac13b18c47e41877e45969ddbd7383587
Will Trufant <will@iceboxinteractive.com>
2024-03-23 09:51:22 -0500

    New / expanded default opts

--- config.sample.php
+++ config.sample.php
@@ -1,19 +1,25 @@
 <?php
 
+// You may need to set your local timezone if unset:
+//date_default_timezone_set('America/Chicago');
+
 // 'PW' => 'User'. I'm guessing that there won't be a lot of users, so manually updating in two places should be easy.
-// If not using mikrotik, the same username needs to be in actions.sh
 
 $pws = array(
-    'ValheimPW' => array('user' => 'VAL', 'expire' => '3 hours'),
-    'A different PW' => array('user' => 'CPC', 'expire' => '1 week'),
+	"Odin's Beard" => array('user' => 'VAL', 'expire' => '3 hours'),
+	'Cr33p3r!' => array('user' => 'MINE', 'expire' => '3 hours'),
+	"It's-a me!" => array('user' => 'ME', 'expire' => '1 week'),
 );
 
-// Firewall configs.
-$FW = ''; // current options: iptables, mikrotik
+$theme = 'lock'; // Current options: lock
+
+// Firewall config.
+$FW = 'mikrotik'; // current options: firewalld, iptables, mikrotik, ufw
 
 $mikrotik = array(
-    'addr' => '192.168.1.1', // Router IP.
-    'protocol' => 'http', // http or https.
-    'user' => 'admin', // User with xxx perms.
-    'pw' => 'adminPW' // User's PW.
+	'proto' => 'http', // http or https.
+	'addr' => '192.168.1.1', // Router IP.
+	'port' => '80', // 443 for default HTTPS, or something custom?
+	'user' => 'admin', // User with xxx perms.
+	'pw' => 'password' // User's PW.
 );
